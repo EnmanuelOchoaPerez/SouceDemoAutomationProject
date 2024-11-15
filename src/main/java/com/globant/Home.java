8f1ffd41675808c3e6eaaa8b52ed4a5624c269f3
EnmanuelOchoaPerez <149899527+EnmanuelOchoaPerez@users.noreply.github.com>
2024-11-14 21:24:11 -0500

    estaba probando
    no mucho avance realmente

--- src/main/java/com/globant/Home.java
+++ src/main/java/com/globant/Home.java
@@ -1,10 +1,22 @@
 package com.globant;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 /**
  * Hello world!
  */
 public class Home {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private WebDriver driver;
+
+    @FindBy(className = "login_logo")
+    public static WebElement loginLogo;
+
+    public Home(WebDriver d){
+        this.driver = d;
+        PageFactory.initElements(d,this);
     }
+
 }
--- src/test/java/com/globant/HomeTest.java
+++ src/test/java/com/globant/HomeTest.java
@@ -1,12 +1,20 @@
 package com.globant;
 
-
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
 
+import java.time.Duration;
+import java.time.Instant;
+
+import static com.globant.Home.*;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
@@ -16,34 +24,34 @@ import static org.testng.Assert.assertTrue;
  */
 public class HomeTest {
 
-        private WebDriver driver;
+    public static final Logger log = Logger.getLogger(HomeTest.class);
+    private WebDriver driver = new ChromeDriver();
 
-        @BeforeClass
-        public void setUp() {
-            driver = new ChromeDriver();
-        }
 
-        @Test
-        public void openGoogle() {
-            driver.get("https://www.google.com");
-
-            String expectedTitle = "Google";
-            String actualTitle = driver.getTitle();
-            assertEquals(actualTitle, expectedTitle, "El título de la página no coincide.");
-        }
-
-        @AfterClass
-        public void tearDown() {
-            // Cerrar el navegador
-            if (driver != null) {
-                driver.quit();
-            }
-        }
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    @BeforeClass
+    public void setUp() {
+        driver.get("https://www.saucedemo.com/");
+        Home hm = new Home(driver);
     }
+
+    @Test
+    public void openGoogle() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(loginLogo));
+        String actualTitle = driver.getTitle();
+        log.info(actualTitle);
+    }
+
+    //@AfterClass
+    //public void tearDown() {
+    // Cerrar el navegador
+    //    if (driver != null) {
+    //        driver.quit();
+    //    }
+    //}
+
+    //@Test
+    //public void shouldAnswerWithTrue() {
+    //    assertTrue(true);
+    //}
 }
